40ae3d5e77474c199933d871fff68c5c0f44ecbd
ssk1328 <shashankgangrade@gmail.com>
2016-08-19 12:32:58 +0530

    clean float stuff

--- gateware/csc/common.py
+++ gateware/csc/common.py
@@ -16,21 +16,13 @@ def saturate(i, o, minimum, maximum):
 def coef(value, cw=None):
     return int(value * 2**cw) if cw is not None else value
 
+
 def rgb_layout(dw):
     return [("r", dw), ("g", dw), ("b", dw)]
 
-def rgb16f_layout(dw):
-    return [("rf", dw), ("gf", dw), ("bf", dw)]
 
 def ycbcr444_layout(dw):
     return [("y", dw), ("cb", dw), ("cr", dw)]
 
 def ycbcr422_layout(dw):
     return [("y", dw), ("cb_cr", dw)]
-
-def pix_layout(dw):
-    return [("pix", dw)]
-
-def pixf_layout(dw):
-    return [("pixf", dw)]
-
