5dabccdf0e670e3114b6c63779461951c1c6b493
Carl Karsten <carl@personnelware.com>
2019-08-15 05:11:00 -0500

    grep generated/csr.h for #define name_ADDR and make a big lump of json.

---
+++ mocserver/grep_addrs.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+# grep generated/csr.h for #define name_ADDR 0xe0009014L
+
+import argparse
+import collections
+import json
+import re
+
+from pprint import pprint
+
+def grep_file(filename, regex):
+
+    def_re = re.compile(regex)
+
+    names=[]
+    with open(filename) as f:
+        for line in f:
+            # print(line)
+            try:
+                names.append( def_re.match(line).groupdict())
+            except AttributeError as e:
+                # print(e)
+                # AttributeError: 'NoneType' object has no attribute 'group'
+                pass
+
+    names = [n['name'].split('_') for n in names]
+
+    return names
+
+
+def test_lines():
+
+    lines = [
+            ['a', 'b0', 'c'],
+            ['a', 'b0', 'd'],
+            ['a', 'b1', 'c'],
+            ['a', 'b1', 'd'],
+            ['a', 'e', ],
+        ]
+
+
+def set_path(d, path, value):
+    for key in path[:-1]:
+        d = d[key]
+    d[path[-1]] = value
+
+def make_dict():
+    return collections.defaultdict(make_dict);
+
+def mk_obj(lines):
+    the_dict = make_dict()
+    for line in lines:
+        if line[-1] in ['RESET', 'ISSUE', 'EN']:
+            continue
+        set_path(the_dict, line, 0)
+
+    return the_dict
+
+def crawl(d):
+
+    # are we on a leaf?
+    if type(d)==int: return
+
+
+    # look for fooN keys
+    keys=[]
+    for k in d:
+        crawl(d[k])
+        if k[-1].isdigit():
+            keys.append(k)
+
+    # consolodate them into foo[0,1,2]
+    keys.sort()
+    for k in keys:
+        # grab the value and remove the fooN item
+        v=d.pop(k)
+        # split into foo and N
+        k,n=k[:-1],k[-1]
+        # make a foo list
+        if n=='0':
+            d[k]=[]
+        # append all the values to the foo list
+        d[k].append(v)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file',
+            default="build/opsis_hdmi2usb_lm32/software/include/generated/csr.h")
+
+    parser.add_argument('--regex',
+        default=r"^#define (?P<name>[\w_]+)_ADDR 0x(?P<addr>[0-9a-f]*)L")
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    lines = grep_file(args.file, args.regex)
+    # lines = test_lines()
+
+    o = mk_obj(lines)
+
+    hdmi=o['CSR']['HDMI']
+    # hdmi = o
+
+    crawl(hdmi)
+
+    j = json.dumps(hdmi, indent=2)
+    print(j)
+
+
+if __name__ == '__main__':
+    main()
+
+
